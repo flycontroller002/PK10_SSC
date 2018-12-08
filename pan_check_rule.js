2144882d075a3ef83aadec1864dcc99dbc67883e
flycontroller002 <39856132+flycontroller002@users.noreply.github.com>
2018-12-08 21:32:37 +0800

    Update pan_check_rule.js

--- pan_check_rule.js
+++ pan_check_rule.js
@@ -82,8 +82,11 @@ function getPanPath(_url, remote_type_start) {
     return url;
 }
 
-
 setInterval(function () {
+    var game_start= localStorage.getItem('game_start');
+        consoleLog('game_start=',game_start);
+    if(game_start=="true"){
     doTask();
     console.log('每隔2分钟检查一次是否需要入单');
+    }
 },120000) //每1分钟请求一次
